27f14836ebcb1b7266c86886cf12ba7383d27f93
Siri <siri@vimsa.mint.14>
2013-05-15 15:08:28 +0200

    Top menu improved

--- controller/main.rb
+++ controller/main.rb
@@ -8,12 +8,43 @@
 #  map '/otherurl'
 #
 # this will force the controller to be mounted on: /otherurl.
+
 class MainController < Controller
   # the index action is called automatically when no other action is specified
   def index
     puts "kom til index----------------------------------"
     @title = 'NP Publications'
     @publication_category = ['Peer-reviewed publications', 'Book and book chapters', 'PhD thesis', 'Master thesis', 'Proceedings', 'Abstracts', 'Posters', 'Report series', 'Brief report series', 'Popular science']
+  end
+
+  def publications
+    form = request.params
+    puts form
+    puts "kom til publications----------------------------------"
+    @publication_category = form['publication_category']
+    puts @publication_category
+    case @publication_category 
+    when 'Peer-reviewed publications'
+        redirect("../sci_papers"){|uri| uri.scheme = 'http'}
+    when 'Book and book chapters'
+        redirect("../books"){|uri| uri.scheme = 'http'}
+    when 'PhD thesis'
+        redirect("../phd_thesis"){|uri| uri.scheme = 'http'}
+    when 'Master thesis'
+        redirect("../master_thesis"){|uri| uri.scheme = 'http'}
+    when 'Proceedings'
+        redirect("../proceedings"){|uri| uri.scheme = 'http'}
+    when 'Abstracts'
+        rendirect("../abstracts"){|uri| uri.scheme = 'http'}
+    when 'Posters'
+        redirect("../posters"){|uri| uri.scheme = 'http'}
+    when 'Report series'
+        redirect("../report_series"){|uri| uri.scheme = 'http'}
+    when 'Brief report series'
+        redirect("../brief_report_series"){|uri| uri.scheme = 'http'}
+    else #'Popular science'
+        redirect("../popular_sci"){|uri| uri.scheme = 'http'}
+    end 
   end
 
   # the string returned at the end of the function is used as the html body
--- controller/sci_papers_controller.rb
+++ controller/sci_papers_controller.rb
@@ -3,18 +3,23 @@ class Sci_papersController < Controller
 layout 'default'
 map '/sci_papers'
 
-def authors
-
+def index
 end
 
-def publication_details
-    
-
+def view_submit_index
+    puts "kom til view submit index---"
 end
+
 
 def authors
     @author = ['Strom, Hallvar (NPI)', 'Steen, Harald (NPI)', 'Koc, Nalan (NPI)', 'Holmen, Kim (NPI)']
 end
+
+
+def publication_details
+    
+end
+
 
 def identificators
     puts "identificators -----"
